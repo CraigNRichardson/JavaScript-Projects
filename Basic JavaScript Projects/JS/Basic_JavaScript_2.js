73503967a4a7ec5a828970f5d288816ff841d3c5
Craig Richardson <craig.n.richardson@gmail.com>
2020-03-10 12:00:12 -0600

    Add JavaScript Project 3

--- Basic JavaScript Projects/JS/Basic_JavaScript_2.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -1,5 +1,3 @@
-function myFunction() {
-    var sentence = "I am learning";
-    sentence += " a lot from this book!";
-    document.getElementById("Concatenate") .innerHTML = sentence;
-}
+function WhichButton(event) {
+    alert("You pressed button: " + event.button)
+  }
---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -0,0 +1,5 @@
+function sumFunction() {
+    var addition = 5 + 2;
+    document.getElementById("Math") .innerHTML = "5 + 2 = " + addition;
+
+}
