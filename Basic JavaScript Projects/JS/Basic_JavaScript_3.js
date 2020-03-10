e4eda1cdca5e26b911b7c8be623ed1c8c0044dc1
Craig Richardson <craig.n.richardson@gmail.com>
2020-03-10 13:43:18 -0600

    Update JS 3 Exercise with Order of Operations problem.

--- Basic JavaScript Projects/JS/Basic_JavaScript_3.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -1,9 +1,4 @@
-function multiplicationFunction() {
-    var multiplication = 333 * 330;
-    document.getElementById("Math") .innerHTML = "333 x 330 = " + multiplication;
-}
-
-function divisionFunction() {
-    var division = 333 / 330;
-    document.getElementById("Math") .innerHTML = "333 / 330 = " + division;
+function multiplemathFunction() {
+    var multiplemath = 8 / 2 * (2+2);
+    document.getElementById("Math") .innerHTML = "8 divided by 2, times 2 + 2 equals " + multiplemath;
 }
