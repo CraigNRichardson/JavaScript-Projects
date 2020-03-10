65627da5bab1f398165415ec6583e299ae6cd3c4
Craig Richardson <craig.n.richardson@gmail.com>
2020-03-10 13:35:10 -0600

    Update JS 3 Project with Multiplication and Division Functions

--- Basic JavaScript Projects/JS/Basic_JavaScript_3.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_3.js
@@ -1,5 +1,9 @@
-function subtractionFunction() {
-    var subtraction = 333 - 330;
-    document.getElementById("Math") .innerHTML = "333 - 330 = " + subtraction;
+function multiplicationFunction() {
+    var multiplication = 333 * 330;
+    document.getElementById("Math") .innerHTML = "333 x 330 = " + multiplication;
+}
 
+function divisionFunction() {
+    var division = 333 / 330;
+    document.getElementById("Math") .innerHTML = "333 / 330 = " + division;
 }
