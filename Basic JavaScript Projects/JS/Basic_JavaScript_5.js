e73c239366bc9dd587ea81d4331bde9d8e75178c
Craig Richardson <craig.n.richardson@gmail.com>
2020-03-11 10:15:56 -0600

    Update JS 5 Exercise

--- Basic JavaScript Projects/JS/Basic_JavaScript_5.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_5.js
@@ -1,1 +1,9 @@
-document.write(typeof true);
+function not_Function () {
+    var x = !(5>10);
+    document.getElementById("Not") .innerHTML = "!(5 > 10) is " + x;
+}
+function aff_Function () {
+    var y = !(20>10);
+    document.getElementById("Aff") .innerHTML = "!(20 > 10) is " + y;
+}
+
