c23f8ae9e60787a7feed75c45db71f18b22daa25
Craig Richardson <craig.n.richardson@gmail.com>
2020-03-11 07:31:02 -0600

    Add JS 4 Project KVP

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_4.js
@@ -0,0 +1,11 @@
+function my_Dictionary() {
+    var ThomasJefferson = {
+        DateOfBirth:"April 13, 1743",
+        DateOfDeath:"July 4, 1826",
+        PlaceOfBirth:"Shadwell, Colony of Virginia",
+        PresidentialTerm:"1801 - 1809",
+        PoliticalParty:"Democratic Republican",
+        PlaceOfBirth:"DRC"
+    };
+    document.getElementById("Dictionary") .innerHTML = ThomasJefferson.PlaceOfBirth;
+}
